f79242b76d017abbb565eff91cabab90b4fd1ee0
sapiyuki <sapiyuki@Hotmail.com>
2017-09-15 16:17:52 +0800

    motify .

--- connect.php
+++ connect.php
@@ -6,6 +6,7 @@ try{
 	$conn = new PDO("mysql:host=$host; dbname=$dbname" , $username , $password);
 	$conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	echo "Connected Success";
+	echo "OK!!";
 } catch (PDOException $e){
 	echo "Fail:" . $e->getMessage();
 }
